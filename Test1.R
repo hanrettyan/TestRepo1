ebce2cf49b8df099f4ae94c85300b78523c3a0be
hanrettyan <47253726+hanrettyan@users.noreply.github.com>
2019-08-22 14:35:36 -0400

    Update Test1.R
    added some more maps. still testing.

--- Test1.R
+++ Test1.R
@@ -20,4 +20,27 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 map_data("world") %>%
-  filert( region == "Mexico", is.na(subregion)) -> baja
+  filter( region == "Mexico", is.na(subregion)) -> baja
+datatable( baja )
+
+ggplot() +
+  geom_polygon( aes(long,lat), fill="grey", data=baja) +
+  geom_point( aes(Longitude,Latitude), data=araptus) +
+  xlab("Longitude") + ylab("Latitude") +
+  coord_map()
+
+names(araptus_sf)
+plot(araptus_sf)
+
+library(leaflet)
+leaflet(araptus) %>%
+  addProviderTiles( providers$Esri.WorldImagery) %>%
+  addCircleMarkers( radius = ~Suitability*10, color="red", label = ~Site)
+
+
+library(leaflet.minicharts)
+leaflet(araptus) %>%
+  addProviderTiles( providers$Esri.WorldImagery) %>%
+  addMinicharts(araptus$Longitude, araptus$Latitude, type="pie", 
+                chartdata=araptus[, c("Males", "Females")],
+                                  width= araptus$Suitability*30)
